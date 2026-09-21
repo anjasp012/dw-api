538e86e7d3701230799fa4a09b93cdcfa8b75e88
Anjas <anjas@Anjass-MBP.lan>
2026-09-21 08:22:37 +0700

    main

--- app/main.py
+++ app/main.py
@@ -63,5 +63,4 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
